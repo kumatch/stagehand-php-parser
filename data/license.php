ff156c8051c9e6819cafb62a0856ebff3b247857
KUMAKURA Yousuke <kumatch@gmail.com>
2009-05-13 16:04:02 +0900

    Added.

---
+++ data/license.php
@@ -0,0 +1,25 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * Copyright (c) 1998-2009 Zend Technologies Ltd. (http://www.zend.com)
+ *
+ * This source file is subject to version 2.00 of the Zend license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following url:
+ * http://www.zend.com/license/2_00.txt.
+ * If you did not receive a copy of the Zend license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@zend.com so we can mail you a copy immediately.
+ */
+
+/*
+* Local Variables:
+* mode: php
+* coding: iso-8859-1
+* tab-width: 4
+* c-basic-offset: 4
+* c-hanging-comment-ender-p: nil
+* indent-tabs-mode: nil
+* End:
+*/
